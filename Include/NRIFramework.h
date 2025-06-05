48013b34ec4e8a0cb5b66a90a22dcd237b0b6ba9
dzhdan <dzhdan@nvidia.com>
2025-06-05 16:51:24 +0800

    Unification

--- Include/NRIFramework.h
+++ Include/NRIFramework.h
@@ -75,6 +75,54 @@
 constexpr nri::VKBindingOffsets VK_BINDING_OFFSETS = {0, 128, 32, 64}; // see CMake
 constexpr bool D3D11_COMMANDBUFFER_EMULATION = false;
 
+// Include everything for demo purposes
+struct NRIInterface
+    : public nri::CoreInterface,
+      public nri::HelperInterface,
+      public nri::LowLatencyInterface,
+      public nri::MeshShaderInterface,
+      public nri::RayTracingInterface,
+      public nri::ResourceAllocatorInterface,
+      public nri::StreamerInterface,
+      public nri::SwapChainInterface,
+      public nri::UpscalerInterface {
+    inline bool HasCore() const {
+        return GetDeviceDesc != nullptr;
+    }
+
+    inline bool HasHelper() const {
+        return CalculateAllocationNumber != nullptr;
+    }
+
+    inline bool HasLowLatency() const {
+        return SetLatencySleepMode != nullptr;
+    }
+
+    inline bool HasMeshShader() const {
+        return CmdDrawMeshTasks != nullptr;
+    }
+
+    inline bool HasRayTracing() const {
+        return CreateRayTracingPipeline != nullptr;
+    }
+
+    inline bool HasResourceAllocator() const {
+        return AllocateBuffer != nullptr;
+    }
+
+    inline bool HasStreamer() const {
+        return CreateStreamer != nullptr;
+    }
+
+    inline bool HasSwapChain() const {
+        return CreateSwapChain != nullptr;
+    }
+
+    inline bool HasUpscaler() const {
+        return CreateUpscaler != nullptr;
+    }
+};
+
 struct SwapChainTexture {
     nri::Fence* acquireSemaphore;
     nri::Fence* releaseSemaphore;
